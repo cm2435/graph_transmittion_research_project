bc898557a59bc7f0a3940b97637dfcbe44fda276
cm2435 <cm2435@batch.ac.uk>
2022-10-18 13:44:48 +0100

    wrote documentation

--- erdos_graph.py
+++ erdos_graph.py
@@ -1,21 +1,26 @@
+from typing import Tuple
 import numpy as np
 import random
 import tqdm
 import multiprocessing
 
+
 class ErdosGraphSimulator(object):
     """ """
 
     def __init__(
-        self, num_nodes: int = 20, num_agents: int = 1, num_timestep_edges: int = 4
+        self, num_nodes: int = 20, num_agents: int = 3, num_timestep_edges: int = 4
     ):
         self.num_nodes = num_nodes
         self.num_agents = num_agents
         self.num_timestep_edges = num_timestep_edges
 
     @property
-    def infection_matrix(self):
-        """ """
+    def infection_matrix(self) -> np.ndarray:
+        """
+        Generates a 1D array of the length of the number of nodes and seeds it
+        with num_agents number of initial infections
+        """
         infection_array = np.zeros(self.num_nodes)
         infected_nodes = []
 
@@ -27,9 +32,10 @@ class ErdosGraphSimulator(object):
 
         return infection_array
 
-    def generate_adj_matrix(self):
+    def generate_adj_matrix(self) -> np.ndarray:
         """
-        Generate
+        Generate a random num_node X num_node adjacency matrix that is seeded with
+        num_timestep_edges connections in another wise sparse graph.
         """
         uninfected_graph = np.zeros((self.num_nodes, self.num_nodes))
         for _ in range(self.num_timestep_edges):
@@ -40,8 +46,21 @@ class ErdosGraphSimulator(object):
 
         return uninfected_graph
 
-    def infect_till_saturation(self, infection_probability: float = 0.5):
-        """ """
+    def infect_till_saturation(
+        self, infection_probability: float = 0.5
+    ) -> Tuple[np.ndarray, int]:
+        """
+        Procedure to measure time to infection saturation for a given set of initial conditions
+        in a graph structure.
+
+        Procedure:
+        1. Randomly sample an adjacency matrix
+        2. If any node edge pairs in the adj matrix are infected, infect their pair with p = infection_probability
+        3. Update the infection matrix with any newly infected nodes by index and update timestep
+
+        4. Iterate 1,2,3 untill infection matrix is saturated, then log the number of timesteps needed
+
+        """
         infection_matrix_list = [self.infection_matrix]
         timesteps = 0
 
@@ -74,18 +93,19 @@ class ErdosGraphSimulator(object):
         return infection_matrix_list[-1], timesteps
 
 
-def simulate_saturation(_ = 1): 
+def simulate_saturation(_=1):
+    # Global function just for the sake of making multiprocessing nice and simple
     x = ErdosGraphSimulator()
     _, iterations = x.infect_till_saturation()
     return iterations
 
-if __name__ == "__main__":
 
+if __name__ == "__main__":
     convergence_steps = []
     with multiprocessing.Pool(processes=multiprocessing.cpu_count() * 2 - 1) as p:
         with tqdm.tqdm(total=10000) as pbar:
             for _ in p.imap_unordered(simulate_saturation, range(0, 10000)):
                 pbar.update()
                 convergence_steps.append(_)
-        
+
     print(np.average(convergence_steps))
