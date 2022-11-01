8a5283884dcaa4b4b85f0e66e0bed3ecf154b9ff
cm2435 <cm2435@batch.ac.uk>
2022-11-01 23:32:44 +0000

    linting

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-import itertools 
-import numpy 
-import random 
+import itertools
+import numpy
+import random
 import multiprocessing
-import tqdm 
-import scipy 
+import tqdm
+import scipy
 
 from graph_structure.erdos_graph import ErdosGraphSimulator
 from structure_generation.adj_matrix_gen import *
@@ -11,31 +11,43 @@ from structure_generation.adj_matrix_gen import *
 
 def simulate_saturation(_=1):
     # Global function just for the sake of making multiprocessing nice and simple
-    x = ErdosGraphSimulator(num_nodes=num_nodes, num_agents=num_initial_agents, structure_name= structure_name)
-    _, iterations, fraction_infected = x.infect_till_saturation(infection_probability= 0.01)
+    x = ErdosGraphSimulator(
+        num_nodes=num_nodes,
+        num_agents=num_initial_agents,
+        structure_name=structure_name,
+    )
+    _, iterations, fraction_infected = x.infect_till_saturation(
+        infection_probability=0.01
+    )
     return iterations, fraction_infected
 
 
 if __name__ == "__main__":
     final_dicts = []
-    global num_initial_agents, num_nodes, structure_name 
+    global num_initial_agents, num_nodes, structure_name
     num_initial_agents, num_nodes, structure_name = 1, 20, "fully_connected"
     simulation_output = []
-    
+
     with multiprocessing.Pool(processes=multiprocessing.cpu_count() * 2 - 1) as p:
         num_simulation_steps = 1000
         with tqdm.tqdm(total=num_simulation_steps) as pbar:
-            for _ in p.imap_unordered(simulate_saturation, range(0, num_simulation_steps)):
+            for _ in p.imap_unordered(
+                simulate_saturation, range(0, num_simulation_steps)
+            ):
                 pbar.update()
                 simulation_output.append(_)
 
     convergence_steps = [x[0] for x in simulation_output]
     saturation_fractions = [x[1] for x in simulation_output]
 
-    #Pad the list to ones to the longest saturation length, find the mean across all simulations and the std at each timestep 
-    padded_list = np.array(list(zip(*itertools.zip_longest(*saturation_fractions, fillvalue=1))))
-    saturation_timestep = np.mean(padded_list, axis = 0)
-    saturation_timestep_std = [np.std(padded_list[:, i]) for i in range(len(padded_list[0]))]
+    # Pad the list to ones to the longest saturation length, find the mean across all simulations and the std at each timestep
+    padded_list = np.array(
+        list(zip(*itertools.zip_longest(*saturation_fractions, fillvalue=1)))
+    )
+    saturation_timestep = np.mean(padded_list, axis=0)
+    saturation_timestep_std = [
+        np.std(padded_list[:, i]) for i in range(len(padded_list[0]))
+    ]
 
     stats_dict = {
         "mean": np.average(convergence_steps),
--- structure_generation/adj_matrix_gen.py
+++ structure_generation/adj_matrix_gen.py
@@ -1,30 +1,29 @@
-import numpy as np 
-import random 
+import numpy as np
+import random
+
 
 class GraphStructureGenerator(object):
-    """
-    """
-    def __init__(self, structure_name : str, num_nodes : int = 20):
+    """ """
+
+    def __init__(self, structure_name: str, num_nodes: int = 20):
         self.structure_name = structure_name
         self.num_nodes = num_nodes
         self.allowed_structures = ["fully_connected", "random_sparse"]
 
     @property
     def adj_matrix(self):
-        '''
-        '''
+        """ """
         graph_mapping = {
-            "fully_connected" : self.generate_fully_connected_graph,
-            "random_sparse" : self.generate_sparse_graph
+            "fully_connected": self.generate_fully_connected_graph,
+            "random_sparse": self.generate_sparse_graph,
         }
         return graph_mapping[self.structure_name]()
-    
-    def generate_fully_connected_graph(self): 
-        """
-        """
+
+    def generate_fully_connected_graph(self):
+        """ """
         return np.ones((self.num_nodes, self.num_nodes))
 
-    def generate_sparse_graph(self, num_edges : int = 5) -> np.ndarray:
+    def generate_sparse_graph(self, num_edges: int = 5) -> np.ndarray:
         """
         Generate a random num_node X num_node adjacency matrix that is seeded with
         num_timestep_edges connections in another wise sparse graph.
