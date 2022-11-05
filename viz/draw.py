f21c55352f28ea130e3c4edf20f73043213118ac
Max Haughton <maxhaughton@maxs-mbp.home>
2022-11-05 22:53:15 +0000

    Draw directed graphs with arrows
    If a directed graph is detected (i.e. an asymmetric adjacency matrix) then use arrows instead of lines.

--- viz/draw.py
+++ viz/draw.py
@@ -14,24 +14,28 @@ def draw_graph(mat: np.array, path: str, label_name = ""):
         N1 -- N2
     }
     """
-    graphString = "graph {"
+
+    assumeUndirected = (mat == mat.T).all()
+
+    graphString = "graph { " if assumeUndirected else "digraph { "
     # neato supports non-overlapping w/ splines
     graphString += "graph [layout=neato, overlap=false, splines=true]"
 
     # If the adjacency matrix is symmetric then it
     # is assumed that is an undirected graph.
 
-    assumeUndirected = (mat == mat.T).all()
     # If we think it's undirected then we only iterate the
     # matrix elements above the diagonal
     explain = "(assumed undirected)" if assumeUndirected else ""
     graphString += f"label = \"{label_name} {explain} \" \n"
     spliced = np.triu(mat) if assumeUndirected else mat
     it = np.nditer(spliced, flags=['multi_index'])
+    # To draw a directed graph properly in DOT a different symbol is needed
+    conString = "--" if assumeUndirected else "->"
     for x in it:
         idx = it.multi_index
         if x == 1:
-            graphString += f"{idx[0]} -- {idx[1]}\n"
+            graphString += f"{idx[0]} {conString} {idx[1]}\n"
         else:
             graphString += f"{idx[0]}\n"
     graphString += "}"
