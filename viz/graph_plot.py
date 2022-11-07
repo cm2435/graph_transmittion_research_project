3429156166a11fbd1c3d3fd222e49da417a9e481
Max Haughton <maxhaughton@maxs-mbp.home>
2022-11-07 22:09:54 +0000

    Correct label texts, add a title with a timestamp.

--- viz/graph_plot.py
+++ viz/graph_plot.py
@@ -1,31 +1,37 @@
-import matplotlib.pyplot as plt 
-import numpy as np 
-import random 
-from pathlib import Path 
+import matplotlib.pyplot as plt
+import numpy as np
+import random
+from pathlib import Path
+
 
 def plot_saturation(
-    saturation_fraction_mean : np.ndarray,
-    saturation_fraction_std : np.ndarray,
-    timesteps : np.array = None, 
-    graph_type : str = "fully_connected",
-    save_filename : bool = None
-    ):
+    saturation_fraction_mean: np.ndarray,
+    saturation_fraction_std: np.ndarray,
+    timesteps: np.array = None,
+    graph_type: str = "fully_connected",
+    save_filename: bool = None
+):
 
-    timesteps = [x for x in range(len(saturation_fraction_mean))] if timesteps is None else timesteps
+    timesteps = [x for x in range(
+        len(saturation_fraction_mean))] if timesteps is None else timesteps
 
-    fig = plt.figure(figsize=(12,8))
-    plt.plot(timesteps, saturation_fraction_mean, label='Stuff')
-    plt.fill_between(timesteps, saturation_fraction_mean+saturation_fraction_std, saturation_fraction_mean-saturation_fraction_std, alpha = 0.3, label = "1.Std across all runs")
-    plt.xlabel(f'timesteps for graph to saturate')
-    plt.ylabel(f'fraction of graph {graph_type} nodes saturated')
+    fig = plt.figure(figsize=(12, 8))
+    plt.plot(timesteps, saturation_fraction_mean, label=f"{graph_type}, saturation")
+    plt.fill_between(timesteps, saturation_fraction_mean+saturation_fraction_std,
+                     saturation_fraction_mean-saturation_fraction_std, alpha=0.3, label="1.Std across all runs")
+    plt.xlabel(f'Number of timesteps')
+    plt.ylabel(f'Fraction saturated')
     plt.legend()
-    #plt.show()
-    if save_filename: 
-        fig.savefig(str(Path(__file__).parents[1] / "data" / "figures") + f'/{graph_type}.png')
+    from datetime import datetime
+    # Move this to somewhere else
+    plt.title(f"{graph_type}, plotted on {str(datetime.now())}")
+    # plt.show()
+    if save_filename:
+        fig.savefig(
+            str(Path(__file__).parents[1] / "data" / "figures") + f'/{graph_type}.png')
 
-    
 
-if __name__ == "__main__": 
+if __name__ == "__main__":
     import seaborn as sns
     import numpy as np
     import matplotlib.pyplot as plt
