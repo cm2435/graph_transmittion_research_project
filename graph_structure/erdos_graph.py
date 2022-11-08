04bfa36bb9841777d8393978762a5fa3bc2b1b75
Max Haughton <maxhaughton@maxs-mbp.home>
2022-11-08 04:59:40 +0000

    Remove dead code in erdos_graph.py
    This should all be deleted at some point, it's not doing what it's named after.

--- graph_structure/erdos_graph.py
+++ graph_structure/erdos_graph.py
@@ -7,43 +7,6 @@ import scipy
 import pandas as pd
 import itertools
 
-
-class GraphStructureGenerator(object):
-    """ """
-
-    def __init__(self, structure_name: str, num_nodes: int = 20):
-        self.structure_name = structure_name
-        self.num_nodes = num_nodes
-        self.allowed_structures = ["fully_connected", "random_sparse"]
-
-    @property
-    def adj_matrix(self):
-        """ """
-        graph_mapping = {
-            "fully_connected": self.generate_fully_connected_graph,
-            "random_sparse": self.generate_sparse_graph,
-        }
-        return graph_mapping[self.structure_name]()
-
-    def generate_fully_connected_graph(self):
-        """ """
-        return np.ones((self.num_nodes, self.num_nodes))
-
-    def generate_sparse_graph(self, num_edges: int = 5) -> np.ndarray:
-        """
-        Generate a random num_node X num_node adjacency matrix that is seeded with
-        num_timestep_edges connections in another wise sparse graph.
-        """
-        uninfected_graph = np.zeros((self.num_nodes, self.num_nodes))
-        for _ in range(num_edges):
-            random_i, random_j = random.randint(0, self.num_nodes - 1), random.randint(
-                0, self.num_nodes - 1
-            )
-            uninfected_graph[random_i][random_j] = 1
-
-        return uninfected_graph
-
-
 class ErdosGraphSimulator(object):
     """ """
 
@@ -54,6 +17,7 @@ class ErdosGraphSimulator(object):
         num_timestep_edges: int = 4,
         structure_name: str = "fully_connected",
     ):
+        from structure_generation.adj_matrix_gen import GraphStructureGenerator
         self.num_nodes = num_nodes
         self.num_agents = num_agents
         self.num_timestep_edges = num_timestep_edges
