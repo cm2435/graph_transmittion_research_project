159db3c5840a6625d48c1b3d8f744edc8aa71072
cm2435 <cm2435@bath.ac.uk>
2023-02-13 16:46:58 +0000

    using yamls rather than .ini files as inis do not parse types :)

--- main_reachability.py
+++ main_reachability.py
@@ -16,44 +16,19 @@ from structure_generation.path_connection_gen import ProceduralGraphGenerator, S
 def logistic(x, A, k, x0):
     return A / (1.0 + np.exp(-k * (x - x0)))
     
-
 if __name__ == "__main__":
     import configparser
     from scipy import stats
     import argparse
-    configuration = configparser.ConfigParser()
-
-    parser = argparse.ArgumentParser(
-        prog="Graph Reachabbility",
-        description="Simulates information propagation on networks",
-        epilog="Written by Charlie Masters and Max Haughton",
-    )
-    parser.add_argument("--csv-dir", dest="csv_dir", help="Where to write a .csv file")
-    parser.add_argument(
-        "--config",
-        dest="config_file",
-        help="Path to a configuration file, default is config.ini",
-        default="config.ini",
-    )
-    subParsers = parser.add_subparsers(
-        title="Some sub-utilities are available", dest="cmd"
-    )
-    graphDebug = subParsers.add_parser(
-        "gen-graph", help="Generate an example graph using each available method"
-    )
-    parsedArgs = parser.parse_args()
-    configuration.read(parsedArgs.config_file)
-    final_dicts = []
-    conf = {k:v for k,v in configuration["reachability"].items()}
-    if parsedArgs.cmd is not None:
-        parsedArgs.func(parsedArgs, configuration)
-        exit()
-
+    import yaml
+    from pathlib import Path
+    conf = yaml.safe_load(Path('config.yml').read_text())['reachability']
+    
     average_degrees, results_dict = [], {k : [] for k in conf['graph_edge_radii']}
     for graph_rad in tqdm.tqdm(conf['graph_edge_radii']):
         mean_degree_simulation_runs = []
-        for repeat in range(conf['num_simulation_repeats']): 
-            graphgen = GraphStructureGenerator(structure_name=conf['structure_name'], num_nodes=conf['nodes'], graph_edge_radius = conf['graph_edge_radii'])
+        for repeat in range(int(conf['num_simulation_repeats'])): 
+            graphgen = GraphStructureGenerator(structure_name=conf['structure_name'], num_nodes=int(conf['nodes']), graph_edge_radius = float(graph_rad))
             graph = graphgen.initial_adj_matrix
             graph_rand = graphgen.get_graph_structure().initial_adj_matrix
             x = ProceduralGraphGenerator(graph)
