283c483bffac6a559b9e97115d9bf799365551c6
cm2435 <cm2435@bath.ac.uk>
2023-02-21 16:59:29 +0000

    large change to adj_matrix_gen.py: THIS DOESNT WORK BY PASSING DISTANCES ANYMORE. This optimises for mean degree which is the exposed variable in the superclass using a hacky newton raphson of building random graphs to approximate the desired mean degree

--- structure_generation/adj_matrix_gen.py
+++ structure_generation/adj_matrix_gen.py
@@ -13,7 +13,7 @@ class GraphGenerator(abc.ABC):
         self.num_nodes: int = num_nodes
 
     @abc.abstractmethod
-    def generate_adj_matrix(self):
+    def generate_graph(self):
         pass
 
     @staticmethod
@@ -28,32 +28,11 @@ class GraphGenerator(abc.ABC):
             )
         except:
             assert False
-
-class BarabasiAlbert(GraphGenerator):
-    """ """
-
-    name = "barabasi_albert"
-
-    def __init__(
-        self,
-        structure_name: str = "barabasi_albert",
-        num_nodes: int = 50,
-        node_degree: int = None,
-    ):
-        super().__init__(structure_name=structure_name, num_nodes=num_nodes)
-        self.node_degree = (node_degree,)
-        self.initial_adj_matrix = self.generate_adj_matrix()
-
-    def generate_adj_matrix(self) -> np.ndarray:
-        import networkx as nx
-
-        if isinstance(self.node_degree, tuple):
-            self.node_degree = self.node_degree[0]
-        graph = nx.barabasi_albert_graph(
-            self.num_nodes, self.node_degree if self.node_degree is not None else 3
-        )
-        return nx.to_numpy_array(graph)
-
+    
+    @staticmethod
+    def _find_mean_degree(graph : nx.classes.graph.Graph):
+        return np.average([val for (node, val) in graph.degree()])
+    
 
 class ConfigurationGraph(GraphGenerator):
     """ """
@@ -72,6 +51,61 @@ class ConfigurationGraph(GraphGenerator):
         return nx.to_numpy_array(graph)
 
 
+class BarabasiAlbert(GraphGenerator):
+    """ """
+
+    name = "barabasi_albert"
+
+    def __init__(
+        self,
+        structure_name: str = "barabasi_albert",
+        num_nodes: int = 50,
+        target_mean_pathlength: float = None,
+        target_mean_degree : float = None 
+    ):
+        super().__init__(structure_name=structure_name, num_nodes=num_nodes)
+        self.target_mean_pathlength = target_mean_pathlength
+        self.target_mean_degree = target_mean_degree
+        
+        self.optim_metric = "target_mean_degree" if target_mean_degree is not None else "target_mean_pathlength"
+        self.initial_adj_matrix = nx.to_numpy_array(self.generate_calibrated_graph())
+
+    def generate_graph(self, num_attachment_nodes : int = 10):
+        graph = nx.barabasi_albert_graph(
+            self.num_nodes, num_attachment_nodes
+        )
+        return graph
+    
+    def generate_calibrated_graph(self, 
+        update_val : int = 15,
+        max_iters : int = 25, 
+        accepted_precision_percentage : float =  0.0005,
+        connecting_edges : float = 20,
+    ): 
+        #This is slow and I would like to optimise it 
+        if self.optim_metric == "target_mean_degree":
+            graph = self.generate_graph(connecting_edges)
+            for i in range(max_iters): 
+                graph = self.generate_graph(np.absolute(int(connecting_edges)))
+                graph_mean_degree = self._find_mean_degree(graph)
+                update_val = update_val/1.5
+                if graph_mean_degree < self.target_mean_degree:
+                    connecting_edges += update_val
+                elif graph_mean_degree > self.target_mean_degree:
+                    connecting_edges -= update_val
+                    
+                if np.absolute(self.target_mean_degree - graph_mean_degree) < accepted_precision_percentage * self.num_nodes: 
+                    return graph
+            return graph 
+    
+    def generate_adj_matrix(self) -> np.ndarray:
+        if isinstance(self.node_degree, tuple):
+            self.node_degree = self.node_degree[0]
+        graph = nx.barabasi_albert_graph(
+            self.num_nodes, self.node_degree if self.node_degree is not None else 3
+        )
+        return nx.to_numpy_array(graph)
+
 class RandomGeometric(GraphGenerator):
     """ """
 
@@ -81,20 +115,43 @@ class RandomGeometric(GraphGenerator):
         self,
         structure_name: str = "random_geometric",
         num_nodes: int = 50,
-        graph_edge_radius: float = None,
+        target_mean_pathlength: float = None,
+        target_mean_degree : float = None 
+
     ):
         super().__init__(structure_name=structure_name, num_nodes=num_nodes)
-        self.graph_edge_radius = graph_edge_radius
-        self.initial_adj_matrix = self.generate_adj_matrix()
+        self.target_mean_pathlength = target_mean_pathlength
+        self.target_mean_degree = target_mean_degree
 
-    def generate_adj_matrix(self) -> np.ndarray:
-        return nx.to_numpy_array(
-            nx.random_geometric_graph(
-                self.num_nodes,
-                0.1 if self.graph_edge_radius is None else self.graph_edge_radius,
-            )
+        self.optim_metric = "target_mean_degree" if target_mean_degree is not None else "target_mean_pathlength"
+        self.initial_adj_matrix = nx.to_numpy_array(self.generate_calibrated_graph())
+
+    def generate_graph(self, graph_edge_radius : float = 0.5) -> np.ndarray:
+        return nx.random_geometric_graph(
+            self.num_nodes, graph_edge_radius
         )
-
+        
+    def generate_calibrated_graph(self, 
+        update_val : int = 5,
+        max_iters : int = 50, 
+        accepted_precision_percentage : float =  0.00025,
+        graph_radius : float = 10,
+    ): 
+        #This is slow and I would like to optimise it 
+        if self.optim_metric == "target_mean_degree":
+            graph = self.generate_graph(graph_radius)
+            for i in range(max_iters): 
+                graph = self.generate_graph(np.absolute(graph_radius))
+                graph_mean_degree = self._find_mean_degree(graph)
+                if np.absolute(self.target_mean_degree - graph_mean_degree) < accepted_precision_percentage * self.num_nodes: 
+                    return graph
+                
+                update_val = update_val/1.5
+                if graph_mean_degree < self.target_mean_degree:
+                    graph_radius += update_val
+                elif graph_mean_degree > self.target_mean_degree:
+                    graph_radius -= update_val
+            return graph 
 
 class SparseErdos(GraphGenerator):
     """ """
@@ -120,37 +177,41 @@ class SparseErdos(GraphGenerator):
 class GraphStructureGenerator(object):
     """ """
 
-    def __init__(self, structure_name: str, num_nodes: int = 50, **kwargs):
+    def __init__(self, 
+        structure_name: str, 
+        num_nodes: int = 50, 
+        **kwargs):
+
         self.num_nodes: int = num_nodes
         self.structure_name = structure_name
 
-        self.graph_edge_radius = kwargs.get("graph_edge_radius", None)
-        self.node_degree = kwargs.get("node_degree", None)
+        self.target_mean_degree = kwargs.get("target_mean_degree", None)
+        self.average_path_length = kwargs.get("average_path_length", None)
+        
+        assert len(kwargs)==1, \
+            f"please pass only one from [node_degree, average_path_length] to build a random graph, {len(kwargs)} was passed"
+        assert self.target_mean_degree is not None or self.average_path_length is not None,\
+            f"to set internal graph parameters, a (single) metric from [target_mean_degree, average_path_length] should be supplied. {kwargs} was passed"
+    
         self.initial_adj_matrix = self.get_graph_structure().initial_adj_matrix
 
     def get_graph_structure(self) -> np.ndarray:
         """ """
         structure_name = self.structure_name
         graph_mapping = {
-            "fully_connected": FullyConnected,
-            "random_sparse": RandomSparse,
             "barabasi_albert": BarabasiAlbert,
-            "configuration": ConfigurationGraph,
+            #"configuration": ConfigurationGraph,
             "random_geometric": RandomGeometric,
-            "sparse_erdos": SparseErdos,
+            #"sparse_erdos": SparseErdos,
         }
-
-        if structure_name == "random_geometric":
-            return graph_mapping[structure_name](
-                num_nodes=self.num_nodes, graph_edge_radius=self.graph_edge_radius
-            )
-        elif structure_name == "barabasi_albert":
-            return graph_mapping[structure_name](
-                num_nodes=self.num_nodes, node_degree=self.node_degree
-            )
-        else:
-            return graph_mapping[structure_name](num_nodes=self.num_nodes)
+        return graph_mapping[structure_name](
+            num_nodes=self.num_nodes, 
+            target_mean_pathlength = self.average_path_length, 
+            target_mean_degree = self.target_mean_degree
+        )
 
 
 if __name__ == "__main__":
-    x = GraphStructureGenerator(structure_name="random_sparse", num_nodes=50)
+    #x = GraphStructureGenerator(structure_name="random_sparse", num_nodes=50, node_degree = 50)
+    x = GraphStructureGenerator(structure_name = "barabasi_albert", num_nodes= 5000, target_mean_degree = 10)
+    print(nx.from_numpy_array(x.initial_adj_matrix))
