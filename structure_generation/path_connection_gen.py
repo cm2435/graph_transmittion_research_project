511f658bf8b4c8bf92345ffbdd1afdc9550cbd23
cm2435 <cm2435@bath.ac.uk>
2023-03-13 21:29:19 +0000

    linting

--- structure_generation/path_connection_gen.py
+++ structure_generation/path_connection_gen.py
@@ -110,15 +110,11 @@ class ProceduralGraphGenerator(object):
             Instance of GraphStructureMutator with the `initial_structure` as the initial structure.
     """
 
-    def __init__(
-        self, initial_graph, num_nodes: int = 1000, num_agents: int = 1
-    ):
+    def __init__(self, initial_graph, num_nodes: int = 1000, num_agents: int = 1):
         self.num_nodes = num_nodes
         self.num_agents = num_agents
         self.initial_graph = initial_graph
-        self.structure_mutator = GraphStructureMutator(
-            initial_structure=initial_graph
-        )
+        self.structure_mutator = GraphStructureMutator(initial_structure=initial_graph)
         random.seed(1234)
 
     @staticmethod
@@ -136,10 +132,12 @@ class ProceduralGraphGenerator(object):
             np.ndarray: 2D array representing the adjacency matrix of the giant graph.
 
         """
-        if isinstance(graph_structure, np.ndarray): 
+        if isinstance(graph_structure, np.ndarray):
             graph_structure = nx.from_numpy_array(graph_structure)
 
-        nx_giant_graph = graph_structure.subgraph(max(nx.connected_components(graph_structure), key=len))
+        nx_giant_graph = graph_structure.subgraph(
+            max(nx.connected_components(graph_structure), key=len)
+        )
         if verbose:
             print(
                 f"""graph structure properties : {nx_giant_graph} average degree {np.average([val for (node, val) in nx_giant_graph.degree()])}"""
@@ -149,18 +147,16 @@ class ProceduralGraphGenerator(object):
 
     @staticmethod
     def _find_network_closeness_centralities(
-        graph : Union[np.ndarray, nx.classes.graph.Graph]
+        graph: Union[np.ndarray, nx.classes.graph.Graph]
     ) -> dict:
         if isinstance(graph, np.ndarray):
             graph = nx.from_numpy_array(graph)
         return nx.closeness_centrality(graph)
-    
+
     @staticmethod
-    def _find_network_node_positions(
-        graph
-    ) -> dict:
+    def _find_network_node_positions(graph) -> dict:
         return nx.get_node_attributes(graph, "pos")
-    
+
     @staticmethod
     def _generate_network_statistics(
         graph: Union[np.ndarray, nx.classes.graph.Graph]
@@ -190,7 +186,7 @@ class ProceduralGraphGenerator(object):
         """
         initial_graph = np.zeros((self.num_nodes, self.num_nodes))
         edges = np.dstack(np.where(giant_graph == 1))[0]
-        
+
         random_edge_x, random_edge_y = edges[random.randint(0, len(edges) - 1)]
         (
             initial_graph[random_edge_x][random_edge_y],
@@ -223,8 +219,12 @@ class ProceduralGraphGenerator(object):
         np.fill_diagonal(final_matrix, 0)
         return final_matrix
 
-    
-    def _make_infection_array(self, largest_subcomponent, structure_type : str, desired_agent_closeness_centrality : int = 5) -> np.ndarray:
+    def _make_infection_array(
+        self,
+        largest_subcomponent,
+        structure_type: str,
+        desired_agent_closeness_centrality: int = 5,
+    ) -> np.ndarray:
         """
         Generates a 1D array of the length of the number of nodes and seeds it
         with num_agents number of initial infections with the agents in the largest
@@ -233,34 +233,46 @@ class ProceduralGraphGenerator(object):
         infection_arr = np.zeros(subcomponent_adj_matrix.shape[0])
         fully_saturated_arr = np.ones(subcomponent_adj_matrix.shape[0])
 
-        #Compute the closeness centrality of all nodes in network, reorder closeness dict by closest to desired_agent_closeness_centrality and chose first N
+        # Compute the closeness centrality of all nodes in network, reorder closeness dict by closest to desired_agent_closeness_centrality and chose first N
         if structure_type == "barabasi_albert":
-            node_closeness_centralities = self._find_network_closeness_centralities(largest_subcomponent)
-            reordered_list = [list(node_closeness_centralities.keys())[idx] \
+            node_closeness_centralities = self._find_network_closeness_centralities(
+                largest_subcomponent
+            )
+            reordered_list = [
+                list(node_closeness_centralities.keys())[idx]
                 for idx in np.argsort(
-                    np.absolute(np.array(list(node_closeness_centralities.values()))-desired_agent_closeness_centrality))
+                    np.absolute(
+                        np.array(list(node_closeness_centralities.values()))
+                        - desired_agent_closeness_centrality
+                    )
+                )
             ]
             reordered_list.reverse()
-            node_closeness_centralities = {k: node_closeness_centralities[k]+desired_agent_closeness_centrality for k in reordered_list}
-            infection_nodes = list(node_closeness_centralities.keys())[:self.num_agents]
+            node_closeness_centralities = {
+                k: node_closeness_centralities[k] + desired_agent_closeness_centrality
+                for k in reordered_list
+            }
+            infection_nodes = list(node_closeness_centralities.keys())[
+                : self.num_agents
+            ]
 
-        #Find position of all nodes in network, find the closest to the 'centre', seed the initial agents as those closest to 0,0
+        # Find position of all nodes in network, find the closest to the 'centre', seed the initial agents as those closest to 0,0
         elif structure_type == "random_geometric":
             node_positions = self._find_network_node_positions(largest_subcomponent)
             distances = []
             for n in node_positions:
                 x, y = node_positions[n]
                 distances.append((x - 0.5) ** 2 + (y - 0.5) ** 2)
-            infection_nodes = np.argsort(distances).tolist()[:self.num_agents]
+            infection_nodes = np.argsort(distances).tolist()[: self.num_agents]
 
-        for node in infection_nodes: 
+        for node in infection_nodes:
             infection_arr[node] = 1
 
         return infection_arr, fully_saturated_arr
 
     def infect_till_saturation(
         self,
-        structure_name : str, 
+        structure_name: str,
         infection_probability: float = 1,
         max_iters: int = 15000,
         modality: str = "irreversable",
@@ -296,8 +308,12 @@ class ProceduralGraphGenerator(object):
         giant_graph, average_degree, degree_list = self._find_giant_structure(
             self.initial_graph, verbose=verbose
         )
-        infection_arr, fully_saturated_arr = self._make_infection_array(giant_graph, structure_name)
-        initial_graph = self._make_initial_structure(giant_graph = nx.to_numpy_array(giant_graph))
+        infection_arr, fully_saturated_arr = self._make_infection_array(
+            giant_graph, structure_name
+        )
+        initial_graph = self._make_initial_structure(
+            giant_graph=nx.to_numpy_array(giant_graph)
+        )
 
         giant_graph = nx.to_numpy_array(giant_graph)
         infection_arr_list = [infection_arr]
@@ -341,16 +357,13 @@ class ProceduralGraphGenerator(object):
             "average_degree": average_degree,
             "num_nodes": len(current_infection_arr),
             "modality": modality,
-            "degree_list" : degree_list
+            "degree_list": degree_list,
         }
         info_dict.update(self._generate_network_statistics(giant_graph))
-        
+
         return (
             infection_matrix_list,
             timesteps_to_full_saturation,
             fraction_infected,
             info_dict,
         )
-
-
-
